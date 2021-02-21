488ca3bd1d03f81ef6f10c7c864c454429d79246
Kyle Chapman <kyleichapman@gmail.com>
2021-02-21 14:00:16 +0200

    Move mesh based on mouse movement

--- src/Viewer.js
+++ src/Viewer.js
@@ -2,6 +2,32 @@ import React, { useRef, useState } from 'react';
 import { Canvas, useFrame } from 'react-three-fiber';
 
 /**
+ * Rotates a mesh using mouse movement.
+ * @param {any} event The event triggered.
+ * @param {any} mesh The mesh to rotate.
+ * @param {boolean} mouseDown `true` if the mouse has been clicked, `false` otherwise.
+ * @param {number} mouseX The x position of the mouse.
+ * @param {number} mouseY The y position of the mouse.
+ */
+function rotate(event, mesh, mouseDown, mouseX, mouseY) {
+  // mouse is not pressed
+  if (!mouseDown) {
+    return;
+  }
+
+  // calculate mouse position change
+  const deltaX = event.clientX - mouseX;
+  const deltaY = event.clientY - mouseY;
+
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+
+  // rotate mesh
+  mesh.current.rotation.x += deltaY / 100;
+  mesh.current.rotation.y += deltaX / 100;
+}
+
+/**
  * Renders a box using the properties given.
  * @param {any} props The properties to use for the box rendering.
  */
@@ -11,21 +37,21 @@ const Box = (props) => {
 
   // keep track of states
   const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
 
-  // on each frame update
-  useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.005;
-  });
+  const [mouseDown, setMouseDown] = useState(false);
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
 
   return (
     <mesh
       { ...props }
       ref={ mesh }
-      scale={ active ? [1.5, 1.5, 1.5] : [1, 1, 1] }
-      onClick={ (e) => setActive(!active) }
-      onPointerOver={ (e) => setHover(true) }
-      onPointerOut={ (e) => setHover(false) }
+      scale={ [1, 1, 1] }
+      onPointerOver={ e => setHover(true) }
+      onPointerOut={ e => setHover(false) }
+      onPointerMove={ e => { setMouseX(e.clientX); setMouseY(e.clientY); rotate(e, mesh, mouseDown, mouseX, mouseY); } }
+      onPointerDown={ e => setMouseDown(true) }
+      onPointerUp={ e => setMouseDown(false) }
     >
       <boxBufferGeometry args={ [1.5, 1.5, 1.5] } />
       <meshStandardMaterial color={ hovered ? 'hotpink' : 'orange' } />
@@ -42,8 +68,7 @@ function Viewer() {
       <ambientLight intensity={ 0.5 } />
       <spotLight position={ [10, 10, 10] } angle={ 0.15 } penumbra={ 1 } />
       <pointLight position={ [-10, -10, -10] } />
-      <Box position={ [-1.2, 0, 0] } />
-      <Box position={ [1.2, 0, 0] } />
+      <Box position={ [0, 0, 0] } />
     </Canvas>
   );
 }
